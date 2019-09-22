17540dc040e180c7738c4cd937e8b98f4aef2058
The Run <TheRun98@users.noreply.github.com>
2019-09-22 01:12:56 -0400

    Update to version 1.111
    Version 1.11 Notes:
         - Minor bug fixes, etc.

--- n2physics.py
+++ n2physics.py
@@ -3,7 +3,7 @@
 # This is a basic calculator script to detect the total net force of an object using mass and
 # acceleration following the equation "F = ma" for Newton's 2nd law in classical mechanics. 
 #
-# Version 1.1
+# Version 1.11
 # Script will be updated periodically for better accessibility and usability of calculator.
 """
 Created on Mon Sep  2 17:50:37 2019
@@ -13,26 +13,27 @@ Created on Mon Sep  2 17:50:37 2019
 import sys
 
 accel = input("Enter acceleration of object in m/s\N{SUPERSCRIPT TWO}: ")
-accel_int = float(accel)
-if accel_int <= -0.00000000001:
+accel_value = float(accel)
+if accel_value <= -0.00000000001:
     yes_no = input("You have put in a value of deceleration. Continue? ")
     if yes_no == "no" or yes_no == "No":
         print("User selected 'no'. Please try again.\n")
-        quit()
+        sys.exit()
     else:
         print("Storing data...")
 else:
     print("Storing data...")
 
 mass = input("Enter mass of object in kilograms: ")
-mass_int = float(mass)
-if mass_int <= 0:
-    print("Mass cannot be 0 kg or lower. Aborting program...")
+mass_value = float(mass)
+if mass_value <= 0:
+    print("Mass cannot be 0 kg or lower. Aborting program...\n")
     sys.exit()
 else:
     print("Thank you. Proceeding to calculating...\n")
     
-object_force = (accel_int * mass_int)
+object_force = (accel_value * mass_value)
 ob_j = str(round(object_force, 3))
 
 print("The total net force of the object is " + ob_j + " N.")
+
