6e3cee4aa59e71f6312c5291df6fadfd0f51ae2e
The Run <TheRun98@users.noreply.github.com>
2020-02-02 12:18:29 -0500

    Update to version 1.2
         - Removed 'sys' module to replace with graceful loops
         - Overhauled code with 'while' loop to make it easier to forgive integer input mistakes
         - Fixed multiple bugs/crashes

--- n2physics.py
+++ n2physics.py
@@ -3,35 +3,41 @@
 # This is a basic calculator script to detect the total net force of an object using mass and
 # acceleration following the equation "F = ma" for Newton's 2nd law in classical mechanics. 
 #
-# Version 1.14
+# Version 1.2
 # Script will be updated periodically for better accessibility and usability of calculator.
 """
 Created on Mon Sep  2 17:50:37 2019
 @author: TheRun98
 """
-import sys
 import time
 
-accel = input("Enter acceleration of object in m/s\N{SUPERSCRIPT TWO}: ")
-accel_value = float(accel)
-if accel_value <= -0.00000000001:
-    yes_no = input("You have put in a value of deceleration. Continue? ")
-    if yes_no == "no" or yes_no == "No":
-        print("User selected '" + yes_no + "'. Please try again.\n")
-        sys.exit()
+while True:
+    accel = input("Enter acceleration of object in m/s\N{SUPERSCRIPT TWO}: ")
+    accel_value = float(accel)
+    if accel_value <= -0.00000000001:
+        yes_no = input("You have put in a value of deceleration. Continue? ")
+        if yes_no == "no" or yes_no == "No":
+            print("User selected '" + yes_no + "'. Please try again.\n")
+            sys.exit()
+        else:
+            print("Storing data...")
+            break
+        continue            
     else:
         print("Storing data...")
-else:
-    print("Storing data...")
-
-mass = input("Enter mass of object in kilograms: ")
-mass_value = float(mass)
-if mass_value <= 0:
-    print("Mass cannot be 0 kg or lower. Aborting program...\n")
-    sys.exit()
-else:
-    print("Calculating total net force...\n")
-    time.sleep(0.5)
+        break
+    continue
+    
+while True:
+    mass = input("Enter mass of object in kilograms: ")
+    mass_value = float(mass)
+    if mass_value <= 0:
+        print("Mass cannot be 0 kg or lower. Please input a positive value.\n")
+        continue
+    else:
+        print("Calculating total net force...\n")
+        time.sleep(0.5)
+        break
     
 object_force = str(round(accel_value * mass_value, 3))
 print("The total net force of the object is " + object_force + " N.")
